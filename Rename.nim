76b7d68b8c27ef718787c85cbf60de051be6baf8
suinua <34801662+suinua@users.noreply.github.com>
2018-05-12 19:07:20 +0900

    Fix javaFont to jeFont and Upload jeFont,beFont
    🍣

--- Rename.nim
+++ Rename.nim
@@ -3,10 +3,10 @@ import os,strutils,re
 if os.existsDir("beFont") == false:
   createDir("newFont")
 #fontフォルダがあれば一括リネーム
-if os.existsDir("javaFont") == false:
+if os.existsDir("jeFont") == false:
   echo "ERROR fontフォルダがありません。"
 else:
-  for file in walkDirRec("javaFont/",{pcFile}):
-    var newFileName = file.replace("unicode_page","glyph").replace("javaFont","beFont")
+  for file in walkDirRec("jeFont/",{pcFile}):
+    var newFileName = file.replace("unicode_page","glyph").replace("jeFont","beFont")
     newFileName = newFileName.replace(newFileName[13..newFileName.len()-5],newFileName[13..newFileName.len()-5].toUpperAscii)
     moveFile(file,newFileName)
