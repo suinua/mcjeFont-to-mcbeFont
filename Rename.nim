bd1bbfff46d168208847aa8d8b70fb9a535a7248
suinua <34801662+suinua@users.noreply.github.com>
2018-05-12 23:14:57 +0900

    Update files
    空のフォルダを残すために

--- Rename.nim
+++ Rename.nim
@@ -2,10 +2,10 @@ import os,strutils,re
 #newFontフォルダが無ければ作成
 if os.existsDir("beFont") == false:
   createDir("beFont")
-#fontフォルダがあれば一括リネーム
 if os.existsDir("jeFont") == false:
   createDir("jeFont")  
 for file in walkDirRec("jeFont/",{pcFile}):
-  var newFileName = file.replace("unicode_page","glyph").replace("jeFont","beFont")
-  newFileName = newFileName.replace(newFileName[13..newFileName.len()-5],newFileName[13..newFileName.len()-5].toUpperAscii)
-  moveFile(file,newFileName)
+  if file != "jeFont/ignore":
+    var newFileName = file.replace("unicode_page","glyph").replace("jeFont","beFont")
+    newFileName = newFileName.replace(newFileName[13..newFileName.len()-5],newFileName[13..newFileName.len()-5].toUpperAscii)
+    moveFile(file,newFileName)
