05b758cbd23053a506ad774efac8982cdd17194d
suinua <34801662+suinua@users.noreply.github.com>
2018-05-12 19:11:39 +0900

    Fix Rename.nim ,exe
    空のフォルダをアップロード出来ないgithubを恨め

--- Rename.nim
+++ Rename.nim
@@ -1,12 +1,11 @@
 import os,strutils,re
 #newFontフォルダが無ければ作成
 if os.existsDir("beFont") == false:
-  createDir("newFont")
+  createDir("beFont")
 #fontフォルダがあれば一括リネーム
 if os.existsDir("jeFont") == false:
-  echo "ERROR fontフォルダがありません。"
-else:
-  for file in walkDirRec("jeFont/",{pcFile}):
-    var newFileName = file.replace("unicode_page","glyph").replace("jeFont","beFont")
-    newFileName = newFileName.replace(newFileName[13..newFileName.len()-5],newFileName[13..newFileName.len()-5].toUpperAscii)
-    moveFile(file,newFileName)
+  createDir("jeFont")  
+for file in walkDirRec("jeFont/",{pcFile}):
+  var newFileName = file.replace("unicode_page","glyph").replace("jeFont","beFont")
+  newFileName = newFileName.replace(newFileName[13..newFileName.len()-5],newFileName[13..newFileName.len()-5].toUpperAscii)
+  moveFile(file,newFileName)
