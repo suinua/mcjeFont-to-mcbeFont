35ec4812805fd09c5d77353d1ffa7832a5df10f6
suinua <34801662+suinua@users.noreply.github.com>
2018-05-13 12:17:18 +0900

    Update Rename.exe and .nim
    add folder select dialog

--- Rename.nim
+++ Rename.nim
@@ -1,11 +1,23 @@
-import os,strutils,re
-#newFontフォルダが無ければ作成
+import os_files.dialog
+
+import os,strutils,re,dialogs
+
+proc system(formatstr: cstring) {.header: "<stdio.h>", importc: "system", varargs.}
+
+echo "select font folder"
+var di:DialogInfo
+di.kind = dkSelectFolder
+di.title = "変換するフォントを選択"
+di.folder = "C:\\"
+di.extension = "rod"
+let fontFolderPath = di.show()
+
 if os.existsDir("beFont") == false:
-  createDir("beFont")
-if os.existsDir("jeFont") == false:
-  createDir("jeFont")  
-for file in walkDirRec("jeFont/",{pcFile}):
-  if file != "jeFont/ignore":
-    var newFileName = file.replace("unicode_page","glyph").replace("jeFont","beFont")
-    newFileName = newFileName.replace(newFileName[13..newFileName.len()-5],newFileName[13..newFileName.len()-5].toUpperAscii)
+  createDir("beFont") 
+
+for file in walkDirRec(fontFolderPath&"/",{pcFile}):
+  if file != fontFolderPath&"/ignore":
+    var newFileName = file.replace("unicode_page","glyph").replace(fontFolderPath,"beFont")
+    newFileName = newFileName.replace(newFileName[newFileName.len()-6..newFileName.len()-5],newFileName[newFileName.len()-6..newFileName.len()-5].toUpperAscii)
     moveFile(file,newFileName)
+system("explorer beFont")
